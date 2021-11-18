07a5f6a0a9287157a480a515eabbe9526841402d
Federico <federico.gibellini@mail.polimi.it>
2021-11-18 15:46:11 +0100

    Update Login Section
    Reorganized starting servlet
    Updated web.xml
    Introduced css

--- db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/CheckLogin.java
+++ db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/CheckLogin.java
@@ -2,29 +2,40 @@ package it.polimi.db2.project.servlet;
 
 import java.io.IOException;
 
+import javax.ejb.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import it.polimi.db2.project.entities.Client;
+import it.polimi.db2.project.services.UserService;
 import it.polimi.db2.project.utils.TemplateEngineHandler;
 
-@WebServlet("/GetLogin")
-public class GetLogin extends HttpServlet {
+@WebServlet("/CheckLogin")
+public class CheckLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private TemplateEngine templateEngine;
+	
+	@EJB
+	private UserService uService;
        
     public void init() throws ServletException {
-    	templateEngine = TemplateEngineHandler.getEngine(getServletContext());
+    	ServletContext sc = getServletContext();
+    	templateEngine = TemplateEngineHandler.getEngine(sc);
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//give access to landing page (page for login or registration)
-		String path = "/WEB-INF/index.html";
+		Client client;
+		client = uService.getClient(request.getParameter("username"), request.getParameter("password"));
+		//give actual access to result page
+		String path = "/WEB-INF/OK.html";
 		ServletContext servletContext = getServletContext();
 		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		ctx.setVariable("username", client.getUsername());
+		ctx.setVariable("email", client.getEmail());
 		templateEngine.process(path, ctx, response.getWriter());
 		
 	}
