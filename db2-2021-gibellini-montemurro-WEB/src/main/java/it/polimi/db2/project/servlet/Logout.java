e75c7721625baa657093a04033c24934569d143b
ElenaMontemurro <elena.montemurro@mail.polimi.it>
2021-12-11 13:10:09 +0100

    Minor fix

--- db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/Logout.java
+++ db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/Logout.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import it.polimi.db2.project.entities.User;
-
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
