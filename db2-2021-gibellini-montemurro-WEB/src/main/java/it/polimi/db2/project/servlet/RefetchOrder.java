bf16543dbd72bab5c973a37d06dff3b6be0da465
Federico <federico.gibellini@mail.polimi.it>
2021-12-21 23:16:34 +0100

    Bug fix
    Fixed privacy leak in RefetchOrder
    Basically introduced error handling for RefetchOrder

--- db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/RefetchOrder.java
+++ db2-2021-gibellini-montemurro-WEB/src/main/java/it/polimi/db2/project/servlet/RefetchOrder.java
@@ -11,9 +11,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.thymeleaf.TemplateEngine;
 
+import it.polimi.db2.project.entities.Client;
 import it.polimi.db2.project.entities.Order;
 import it.polimi.db2.project.services.OrderService;
 import it.polimi.db2.project.utils.TemplateEngineHandler;
+import it.polimi.db2.project.utils.Error;
 
 @WebServlet("/RefetchOrder")
 public class RefetchOrder extends HttpServlet {
@@ -31,11 +33,34 @@ public class RefetchOrder extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		//get id order from the parameter
-		int orId = Integer.parseInt(request.getParameter("orId"));
+		Order order = null;
 		
-		//get order from just-found id
-		Order order = os.findOrderById(orId);
+		//try to get order
+		try {
+		int orId = Integer.parseInt(request.getParameter("orId"));
+		order = os.findOrderById(orId);
+		} catch(Exception e) {
+			Error error = new Error(HttpServletResponse.SC_BAD_REQUEST, "Illegal parameter passed");
+			error.forward("/GetUserHomePage", this, request, response);
+			return;
+		}
+		
+		//check user has the rights to fetch specified order
+		try {
+			Client user = (Client)request.getSession().getAttribute("user");
+			if(!user.getUsername().equals(order.getSubscription().getUser().getUsername())) throw new IllegalArgumentException();
+		} catch (Exception e) {
+			Error error = new Error(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized resource access");
+			error.forward("/GetUserHomePage", this, request, response);
+			return;
+		}
+		
+		//check the order is actually to refetch
+		if(order.getValidity()) {
+			Error error = new Error(HttpServletResponse.SC_BAD_REQUEST, "Illegal request");
+			error.forward("/GetUserHomePage", this, request, response);
+			return;
+		}
 		
 		//save order in session
 		request.getSession().setAttribute("order", order);
